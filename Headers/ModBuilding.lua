ee924538d8476636b63a0efd691e1f9ed920d977
VirRus77 <virrus77@gmail.com>
2022-11-10 00:05:32 +0300

    Add intherit methods ModCustom.

--- Headers/ModBuilding.lua
+++ Headers/ModBuilding.lua
@@ -54,7 +54,7 @@ end
 ---@param TL? integer[] Top Left ground coordinates of the building (for area) e.g. (-1,-1) - Defaults to none
 ---@param BR? integer[] Bottom Right ground coordinates of the building (for area) e.g. (2,0) - Defaults to none
 ---@param Access? integer[] Access point ground coordinates of the building e.g. (1,1) - Defaults to none
----@param UsingCustomModel boolean True if using a custom model, false if using in game model/default model - Defaults to true
+---@param UsingCustomModel? boolean True if using a custom model, false if using in game model/default model - Defaults to true
 function ModBuilding.CreateBuilding ( UniqueName, NewIngredientsStringArr, NewIngredientsAmountArr, ModelName, TL, BR, Access, UsingCustomModel )
 end
 
@@ -237,5 +237,68 @@ end
 ---@param EndX integer
 ---@param EndY integer
 function ModBuilding.UnregisterForNewBuildingInAreaCallback ( StartX, StartY, EndX, EndY )
+end
+
+----- ModCustom -----
+
+--- Register a callback on specific events for a custom item
+-- Can be used in functions: Creation() 
+-- Version: 135.18
+---@param UniqueName string The unique name of the custom item - Required
+---@param CallbackType string The type of callback to register for - Required [FoodConsumed, ClothingTopAdded, ClothingTopRemoved, ClothingHatAdded, ClothingHatRemoved, ConverterCreateItem, ConverterComplete, HoldablePickedUp, HoldableDroppedOnGround, AddedToConverter]
+---@param Callback function
+function ModBuilding.RegisterForCustomCallback ( UniqueName, CallbackType, Callback )
+end
+
+--- Update custom model parameters (All parameters - scale, rotation and translation)
+-- Can be used in functions: Creation() 
+-- Version: 134.30
+---@param UniqueName string The unique name of the custom item - Required
+---@param Scale number Scale of the model - Defaults to 1
+---@param RotX number Rotation (X) of the model - Defaults to 0
+---@param RotY number Rotation (Y) of the model - Defaults to 0
+---@param RotZ number Rotation (Z) of the model - Defaults to 0
+---@param TransX number Translation (X) of the model - Defaults to 0
+---@param TransY number Translation (Y) of the model - Defaults to 0
+---@param TransZ number Translation (Z) of the model - Defaults to 0
+function ModBuilding.UpdateModelParameters ( UniqueName, Scale, RotX, RotY, RotZ, TransX, TransY, TransZ )
+end
+
+--- Update custom model rotation only
+-- Can be used in functions: Creation() 
+-- Version: 134.30
+---@param UniqueName string The unique name of the custom item - Required
+---@param RotX number Rotation (X) of the model - Defaults to 0
+---@param RotY number Rotation (Y) of the model - Defaults to 0
+---@param RotZ number Rotation (Z) of the model - Defaults to 0
+function ModBuilding.UpdateModelRotation ( UniqueName, RotX, RotY, RotZ )
+end
+
+--- Update custom model scale only
+-- Can be used in functions: Creation() 
+-- Version: 134.30
+---@param UniqueName string The unique name of the custom item - Required
+---@param Scale number Scale of the model - Defaults to 1
+function ModBuilding.UpdateModelScale ( UniqueName, Scale )
+end
+
+--- Update custom model scale only (Defining each of the axes)
+-- Can be used in functions: Creation() 
+-- Version: 134.30
+---@param UniqueName string The unique name of the custom item - Required
+---@param ScaleX number X Scale of the model - Defaults to 1
+---@param ScaleY number Y Scale of the model - Defaults to 1
+---@param ScaleZ number Z Scale of the model - Defaults to 1
+function ModBuilding.UpdateModelScaleSplit ( UniqueName, ScaleX, ScaleY, ScaleZ )
+end
+
+--- Update custom model translation only
+-- Can be used in functions: Creation() 
+-- Version: 134.30
+---@param UniqueName string The unique name of the custom item - Required
+---@param TransX number Translation (X) of the model - Defaults to 0
+---@param TransY number Translation (X) of the model - Defaults to 0
+---@param TransZ number Translation (X) of the model - Defaults to 0
+function ModBuilding.UpdateModelTranslation ( UniqueName, TransX, TransY, TransZ )
 end
 
--- Headers/ModDecorative.lua
+++ Headers/ModDecorative.lua
@@ -11,3 +11,66 @@ ModDecorative = { }
 function ModDecorative.CreateDecorative ( UniqueName, NewIngredientsStringArr, NewIngredientsAmountArr, ModelName, UsingCustomModel )
 end
 
+----- ModCustom -----
+
+--- Register a callback on specific events for a custom item
+-- Can be used in functions: Creation() 
+-- Version: 135.18
+---@param UniqueName string The unique name of the custom item - Required
+---@param CallbackType string The type of callback to register for - Required [FoodConsumed, ClothingTopAdded, ClothingTopRemoved, ClothingHatAdded, ClothingHatRemoved, ConverterCreateItem, ConverterComplete, HoldablePickedUp, HoldableDroppedOnGround, AddedToConverter]
+---@param Callback function
+function ModDecorative.RegisterForCustomCallback ( UniqueName, CallbackType, Callback )
+end
+
+--- Update custom model parameters (All parameters - scale, rotation and translation)
+-- Can be used in functions: Creation() 
+-- Version: 134.30
+---@param UniqueName string The unique name of the custom item - Required
+---@param Scale number Scale of the model - Defaults to 1
+---@param RotX number Rotation (X) of the model - Defaults to 0
+---@param RotY number Rotation (Y) of the model - Defaults to 0
+---@param RotZ number Rotation (Z) of the model - Defaults to 0
+---@param TransX number Translation (X) of the model - Defaults to 0
+---@param TransY number Translation (Y) of the model - Defaults to 0
+---@param TransZ number Translation (Z) of the model - Defaults to 0
+function ModDecorative.UpdateModelParameters ( UniqueName, Scale, RotX, RotY, RotZ, TransX, TransY, TransZ )
+end
+
+--- Update custom model rotation only
+-- Can be used in functions: Creation() 
+-- Version: 134.30
+---@param UniqueName string The unique name of the custom item - Required
+---@param RotX number Rotation (X) of the model - Defaults to 0
+---@param RotY number Rotation (Y) of the model - Defaults to 0
+---@param RotZ number Rotation (Z) of the model - Defaults to 0
+function ModDecorative.UpdateModelRotation ( UniqueName, RotX, RotY, RotZ )
+end
+
+--- Update custom model scale only
+-- Can be used in functions: Creation() 
+-- Version: 134.30
+---@param UniqueName string The unique name of the custom item - Required
+---@param Scale number Scale of the model - Defaults to 1
+function ModDecorative.UpdateModelScale ( UniqueName, Scale )
+end
+
+--- Update custom model scale only (Defining each of the axes)
+-- Can be used in functions: Creation() 
+-- Version: 134.30
+---@param UniqueName string The unique name of the custom item - Required
+---@param ScaleX number X Scale of the model - Defaults to 1
+---@param ScaleY number Y Scale of the model - Defaults to 1
+---@param ScaleZ number Z Scale of the model - Defaults to 1
+function ModDecorative.UpdateModelScaleSplit ( UniqueName, ScaleX, ScaleY, ScaleZ )
+end
+
+--- Update custom model translation only
+-- Can be used in functions: Creation() 
+-- Version: 134.30
+---@param UniqueName string The unique name of the custom item - Required
+---@param TransX number Translation (X) of the model - Defaults to 0
+---@param TransY number Translation (X) of the model - Defaults to 0
+---@param TransZ number Translation (X) of the model - Defaults to 0
+function ModDecorative.UpdateModelTranslation ( UniqueName, TransX, TransY, TransZ )
+end
+
